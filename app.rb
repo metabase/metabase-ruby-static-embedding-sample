c214ed28946fdbed4d3bdc7cb47c95e7d5d623fa
Alberto Perdomo <alberto.perdomo@gmail.com>
2023-12-27 17:59:48 +0000

    Initial commit

---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'jwt'
+
+METABASE_EMBEDDING_SECRET = ENV.fetch('METABASE_EMBEDDING_SECRET')
+METABASE_SITE_URL = ENV.fetch('METABASE_SITE_URL') || 'http://localhost:3000'
+METABASE_EMBED_DASHBOARD_ID= ENV.fetch('METABASE_EMBED_DASHBOARD_ID')
+
+get '/' do
+  payload = {
+    :resource => {:dashboard => 8},
+    :params => {          
+    },
+    :exp => Time.now.to_i + (60 * 10) # 10 minute expiration
+  }
+  token = JWT.encode payload, METABASE_EMBEDDING_SECRET
+  iframe_url = METABASE_SITE_URL + "/embed/dashboard/" + token + "#bordered=true&titled=true"
+
+  <<~TEXT
+  <iframe
+    src="#{iframe_url}"
+    frameborder="0"
+    width="800"
+    height="600"
+    allowtransparency>
+  </iframe>
+  TEXT
+end
